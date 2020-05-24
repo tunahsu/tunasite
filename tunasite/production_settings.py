92c8f75e4f603a11e666dc72e0515f24f9be2e0e
Tuna Hsu <tunahsu45@gmail.com>
2020-05-24 21:16:20 +0800

    Modified production settings

--- tunasite/production_settings.py
+++ tunasite/production_settings.py
@@ -1,6 +1,7 @@
 import dj_database_url
 from tunasite.settings import *
 
+STATIC_ROOT = 'staticfiles'
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 DEBUG = False
